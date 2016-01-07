e39110feef70a6bb29175fce821f2c2e43a46adf
Andrew Chen <sixpercentninja@andrews-macbook-pro.local>
2016-01-06 16:21:58 -0800

    Initial Commit

---
+++ TaxCalc/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TaxCalc
+//
+//  Created by Andrew Chen on 1/6/16.
+//  Copyright © 2016 Andrew Chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TaxCalc/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TaxCalc
+//
+//  Created by Andrew Chen on 1/6/16.
+//  Copyright © 2016 Andrew Chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
